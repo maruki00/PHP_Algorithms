7649a3e57adb1f5162350a577d26eccce27560c4
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-02-02 05:06:15 +0300

    insertion

---
+++ 1-sorting/insertion.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Insertion{
+    public function __construct(
+        private array $items
+    ){}
+
+    public final function sort():array
+    {
+        for($i=1; $i<count($this->items); $i++){
+            $j = $i;
+            while($j>0 && $this->items[$j] > $this->items[$j-1]){
+                $tmp = $this->items[$j];
+                $this->items[$j] = $this->items[$j-1];
+                $this->items[$j-1] = $tmp;
+            }
+        }
+        return $this->items;
+    }
+}
+
+$i = new Insertion([1,4,2,5,67,8,4,5678,44,45,6,2345,2456,3456]);
+print_r($i->sort());
